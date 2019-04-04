861feb66f283132691d2a1ed5897670db5ed38f0
Michael Olmsted <molmsted98@gmail.com>
2019-04-04 02:01:18 -0400

    Basic login screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,10 +1,34 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  Text, Button, View
+  Button, View, TouchableWithoutFeedback, Keyboard
 } from 'react-native';
-import { Action } from '../helpers/Navigation';
+import {TextField} from 'react-native-materialui-textfield';
+import {Action} from '../helpers/Navigation';
 
 export default class LoginScreen extends Component {
+  componentDidMount() {
+    this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this.keyboardDidHide);
+  }
+
+  componentWillUnmount() {
+    this.keyboardDidHideListener.remove();
+  }
+
+  keyboardDidHide = () => {
+    if (this.usernameInput.focused) {
+      this.usernameInput.blur();
+    }
+    if (this.passwordInput.focused) {
+      this.passwordInput.blur();
+    }
+  };
+
+  validateInputs = (text) => {
+    if (text.contains(" ") || text.length === 0) {
+      return
+    }
+    return '';
+  };
 
   validateAndLogin = () => {
     this.props.navigation.dispatch(Action.Login);
@@ -12,13 +36,37 @@ export default class LoginScreen extends Component {
 
   render() {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>Login</Text>
-          <Button
-              onPress={() => this.validateAndLogin()}
-              title="Login"
-          />
-        </View>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+          <View style={{flex: 1, justifyContent: 'center', padding: 24}}>
+            <TextField
+                ref={(component) => {
+                  this.usernameInput = component
+                }}
+                label="Username"
+                returnKeyType="next"
+                autocorrect="false"
+                onSubmitEditing={() => {
+                  this.passwordInput.focus();
+                }}
+                onChangeText={text => this.setState({usernameText: text})}
+            />
+            <TextField
+                ref={(component) => {
+                  this.passwordInput = component
+                }}
+                label="Password"
+                autocorrect="false"
+                onChangeText={text => this.setState({passwordText: text})}
+            />
+
+            <View style={{marginTop: 32}}>
+              <Button
+                  onPress={() => this.validateAndLogin()}
+                  title="Login"
+              />
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
     );
   }
 }
